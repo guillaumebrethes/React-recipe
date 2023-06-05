1df4c896b04f41179cd03fcba76a7de24313a632
guillaumebrethes <herver86@gmail.com>
2023-06-05 22:08:31 +0200

    🚧 work in progress✨: veggie/component api ok, but API crash ??

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -10,10 +10,17 @@ const Popular = () => {
   const [popular, setPopular] = useState([]);
 
   const getPopular = async () => {
-    const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
-    const data = await api.json();
-    setPopular(data.recipes);
-    console.log('data.recipes => ', data.recipes);
+    const check = localStorage.getItem('popular');
+
+    if (check) {
+      setPopular(JSON.parse(check));
+    }
+    else {
+      const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
+      const data = await api.json();
+      localStorage.setItem('popular', JSON.stringify(data.recipes));
+      setPopular(data.recipes);
+    }
   };
 
   useEffect(() => {
@@ -24,13 +31,22 @@ const Popular = () => {
     <div>
       <Wrapper>
         <h3>Popular Picks</h3>
-        <Splide>
+        <Splide
+          options={{
+            perPage: 4,
+            arrows: true,
+            pagination: true,
+            drag: 'free',
+            gap: '3rem',
+          }}
+        >
           {
             popular.map((recipe) => (
-              <SplideSlide>
+              <SplideSlide key={recipe.id}>
                 <Card>
                   <p>{recipe.title}</p>s
                   <img src={recipe.image} alt={recipe.title} />
+                  <Gradient />
                 </Card>
               </SplideSlide>
             ))
@@ -42,12 +58,46 @@ const Popular = () => {
 };
 
 const Wrapper = styled.div`
-margin: 4rem 0rem
+padding: 2rem
 `;
 
 const Card = styled.div`
 min-height: 25rem;
 overflow: hidden;
+border-radius: 2rem;
+position: relative;
+
+img {
+  border-radius: 2rem;
+  position: absolute;
+  left: 0;
+  width: 100%
+  height: 100%
+  objectif-fit:cover
+}
+p{
+  position: absolute;
+  z-index:10;
+  left: 50%;
+  bottom: 0%;
+  transform: translate(-50%,0%);
+  color: white;
+  witdh: 100%;
+  text-align: center;
+  font-weight:600;
+  font-size: 1rem;
+  height: 40%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+`;
+const Gradient = styled.div`
+z-index:3;
+position: absolute;
+width: 100%;
+height: 100%;
+background : linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.5));
 `;
 
 export default Popular;
